d1cae3c6fd4e213ea54e14c76fce708142e90ed4
WeiJ-Cheng <a26591349@gmail.com>
2025-03-28 18:38:20 +0800

    Initial commit: Baby Names Trend Visualizer

---
+++ webcrawler.py
@@ -0,0 +1,45 @@
+"""
+File: webcrawler.py
+Name: 
+--------------------------
+This file collects more data from
+https://www.ssa.gov/oact/babynames/decades/names2010s.html
+https://www.ssa.gov/oact/babynames/decades/names2000s.html
+https://www.ssa.gov/oact/babynames/decades/names1990s.html
+Please print the number of top200 male and female on Console
+You should see:
+---------------------------
+2010s
+Male Number: 10905209
+Female Number: 7949058
+---------------------------
+2000s
+Male Number: 12979118
+Female Number: 9210073
+---------------------------
+1990s
+Male Number: 14146775
+Female Number: 10644698
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    for year in ['2010s', '2000s', '1990s']:
+        print('---------------------------')
+        print(year)
+        url = 'https://www.ssa.gov/oact/babynames/decades/names'+year+'.html'
+        
+        response = requests.get(url)
+        html = response.text
+        soup = BeautifulSoup(html)
+
+        # ----- Write your code below this line ----- #
+
+        pass
+
+
+if __name__ == '__main__':
+    main()
